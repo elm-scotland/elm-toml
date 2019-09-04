c2a89aa2393d3e17e69e3b87e73a6c3b6cb90373
Rupert Smith <rupert@thesett.com>
2019-09-04 19:47:39 +0100

    Sketched out the data model for TOML to parse into.

--- src/TOML/Decode.elm
+++ src/TOML/Decode.elm
@@ -3,3 +3,40 @@ module TOML.Decode exposing (Decoder(..))
 
 type Decoder
     = Decoder
+
+
+
+--
+
+
+type alias TomlTable =
+    List ( String, TomlValue )
+
+
+type TomlBasic
+    = BString String
+    | BInt Int
+    | BFloat Float
+    | BBoolean Bool
+    | BDateTime String
+    | BLocalDateTime String
+    | BLocalDate String
+    | BLocalTime String
+
+
+type TomlList
+    = LString (List String)
+    | LInt (List Int)
+    | LFloat (List Float)
+    | LDateTime (List String)
+    | LLocalDateTime (List String)
+    | LLocalDate (List String)
+    | LLocalTime (List String)
+    | LList (List TomlList)
+    | LTable (List TomlTable)
+
+
+type TomlValue
+    = VBasic TomlBasic
+    | VList TomlList
+    | VTable String TomlTable
