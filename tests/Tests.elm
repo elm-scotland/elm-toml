9a4e0fc2b7ae370539440cccadec047ae22e7e4a
T.S. Foster <hi@tsf.io>
2020-02-05 19:59:21 +0000

    Add more tests from the TOML spec

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,15 +1,37 @@
 module Tests exposing (suite)
 
 import Expect
-import TOML.Parser exposing (..)
-import Test exposing (..)
+import TOML.Parser exposing (TomlList(..), TomlValue(..), fromString)
+import Test exposing (Test, describe, test)
 
 
 suite : Test
 suite =
     describe "TOML.Parser"
-        [ describe "Booleans"
-            [ test "Spec 1" <|
+        [ describe "Comments"
+            [ test "Spec comments example 1" <|
+                \_ ->
+                    fromString "# This is a full-line comment\nkey = \"value\" # This is a comment at the end of a line"
+                        |> Expect.equal (Ok [ ( "key", VString "value" ) ])
+            ]
+        , describe "Key/Value Pair"
+            [ test "Spec key/value pair example 1" <|
+                \_ ->
+                    fromString "key = \"value\""
+                        |> Expect.equal (Ok [ ( "key", VString "value" ) ])
+            , test "Spec key/value pair example 2" <|
+                \_ ->
+                    fromString "key = # INVALID"
+                        |> Expect.err
+            ]
+        , describe "String"
+            [ test "Spec String example 1" <|
+                \_ ->
+                    fromString "str = \"I'm a string. \\\"You can quote me\\\". Name\\tJos\\u00E9\\nLocation\\tSF.\""
+                        |> Expect.equal (Ok [ ( "str", VString "I'm a string. \"You can quote me\". Name\tJosé\nLocation\tSF." ) ])
+            ]
+        , describe "Booleans"
+            [ test "Spec Boolean example 1" <|
                 \_ ->
                     fromString "bool1 = true\nbool2 = false"
                         |> Expect.equal (Ok [ ( "bool1", VBoolean True ), ( "bool2", VBoolean False ) ])
